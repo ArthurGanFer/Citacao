c43effd83d738eaedf45a4df7adcb899544c2fbf
31338283 <31338283@099222-P31S303.macklabs.br>
2015-09-09 10:12:02 -0300

    Inicializando repo

---
+++ src/com/br/citacoes/controller/Controller.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.br.citacoes.controller;
+
+/**
+ *
+ * @author 31338283
+ */
+public class Controller {
+    
+    private String driver = "org.apache.derby.jdbc.ClientDriver";
+    private String db = "";
+    private String user = "";
+    private String pwd = "arthur121";
+    
+    public Controller() {
+        connectDB();
+    }
+
+    public void connectDB() {
+
+    }
+
+}
