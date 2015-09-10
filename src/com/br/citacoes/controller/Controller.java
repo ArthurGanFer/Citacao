029a4dd38bc0f7d2f972026d79638cdaf5714d1b
31338283 <31338283@093067-P31S405.macklabs.br>
2015-09-10 09:01:34 -0300

    Implementando CRUD

--- src/com/br/citacoes/controller/Controller.java
+++ src/com/br/citacoes/controller/Controller.java
@@ -23,13 +23,16 @@ public class Controller {
     private String driver = "org.apache.derby.jdbc.ClientDriver";
     private String protocolo = "jdbc:derby:";
     private String dominio = "//localhost:1527/";
-    private String db = "citacaodb";
+    private String db = "citacaoDB";
     private String user = "arthurganfer";
     private String pwd = "arthur121";
     private Connection conn;
 
     public Controller() {
-        connectDB();
+
+        Usuario u = new Usuario(2, "arthur", "arth123");
+        updateSenha(u);
+        System.out.println("----");
         searchUsers();
     }
 
@@ -39,7 +42,7 @@ public class Controller {
             Class.forName(driver).newInstance();
 
             //estabelece conexao
-            conn = DriverManager.getConnection(protocolo + dominio + db + user + pwd);
+            conn = DriverManager.getConnection(protocolo + dominio + db, user, pwd);
 
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
             Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
@@ -47,12 +50,14 @@ public class Controller {
     }
 
     public void searchUsers() {
+        connectDB();
+
         String sql = "SELECT * FROM Usuario";
         try {
             PreparedStatement ps = conn.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
 
-            while (!rs.next()) {
+            while (rs.next()) {
                 int id = rs.getInt("id_usuario");
                 String nome = rs.getString("nome");
                 String senha = rs.getString("senha");
@@ -66,6 +71,82 @@ public class Controller {
         } catch (SQLException ex) {
             Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    public void insertUser(Usuario usuario) {
+        connectDB();
+
+        String sql = "INSERT INTO usuario(nome, senha) VALUES(?,?)";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, usuario.getNome());
+            ps.setString(2, usuario.getSenha());
+            int resposta = ps.executeUpdate();
+
+            if (resposta == 0) {
+                System.out.println("DEU RUIM na inserção");
+            } else {
+                System.out.println("usuario inserido com sucesso");
+            }
+
+            ps.close();
+            conn.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void updateSenha(Usuario usuario) {
+        connectDB();
+
+        String sql = "UPDATE usuario SET nome=?, senha=? WHERE id_usuario=?";
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, usuario.getNome());
+            ps.setString(2, usuario.getSenha());
+            ps.setInt(3, usuario.getIdUsuario());
+            int resposta = ps.executeUpdate();
+
+            if (resposta == 0) {
+                System.out.println("DEU RUIM O UPDATE");
+            } else {
+                System.out.println("Senha modificada");
+            }
+
+            ps.close();
+            conn.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void deleteUsuario(Usuario usuario) {
+        connectDB();
+
+        String sql = "DELETE FROM usuario WHERE id_usuario=?";
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, usuario.getIdUsuario());
+
+            int resposta = ps.executeUpdate();
+
+            if (resposta == 0) {
+                System.out.println("DEU RUIM o delete");
+            } else {
+                System.out.println("Usuario deletado");
+            }
+
+            ps.close();
+            conn.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
 }
